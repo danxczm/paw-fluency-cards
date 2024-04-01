9d31e4462cbd1363b348d0c97913d967217c3935
dan <81313913+danxczm@users.noreply.github.com>
2024-04-01 19:22:06 +0300

    feat: working on flash cards list

--- app/content/page.tsx
+++ app/content/page.tsx
@@ -6,6 +6,7 @@ import { deleteFlashCard, readFlashCard, updateFlashCard } from './actions';
 
 import ContentForm from '@/components/content/flash-card-form';
 import { Button } from '@/components/ui/button';
+import { HoverEffect } from '@/components/ui/card-hover-effect';
 
 const Content = async () => {
   const { data: userSession } = await readUserSession();
@@ -15,12 +16,51 @@ const Content = async () => {
 
   const { data: flashCards } = await readFlashCard();
 
+  const projects = [
+    {
+      title: 'Stripe',
+      description: 'A technology company that builds economic infrastructure for the internet.',
+      link: 'https://stripe.com',
+    },
+    {
+      title: 'Netflix',
+      description:
+        'A streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices.',
+      link: 'https://netflix.com',
+    },
+    {
+      title: 'Google',
+      description:
+        'A multinational technology company that specializes in Internet-related services and products.',
+      link: 'https://google.com',
+    },
+    {
+      title: 'Meta',
+      description:
+        "A technology company that focuses on building products that advance Facebook's mission of bringing the world closer together.",
+      link: 'https://meta.com',
+    },
+    {
+      title: 'Amazon',
+      description:
+        'A multinational technology company focusing on e-commerce, cloud computing, digital streaming, and artificial intelligence.',
+      link: 'https://amazon.com',
+    },
+    {
+      title: 'Microsoft',
+      description:
+        'A multinational technology company that develops, manufactures, licenses, supports, and sells computer software, consumer electronics, personal computers, and related services.',
+      link: 'https://microsoft.com',
+    },
+  ];
+
   return (
-    <section className='h-screen p-6'>
+    <section className='mx-auto max-w-7xl p-8'>
       <div className='flex'>
         <ContentForm />
       </div>
-      {flashCards?.map((flashCard, index) => {
+      <HoverEffect items={projects} className='mt-4' />
+      {/* {flashCards?.map((flashCard, index) => {
         const deleteFlashCardById = deleteFlashCard.bind(null, flashCard.id);
         const updateFlashCardById = updateFlashCard.bind(null, flashCard.id, 'new string');
         return (
@@ -35,7 +75,7 @@ const Content = async () => {
             </form>
           </div>
         );
-      })}
+      })} */}
     </section>
   );
 };
