048c072bc90cd028bc5070e633743de6e7d56670
dan <81313913+danxczm@users.noreply.github.com>
2024-04-11 11:51:56 +0300

    style: changes for flash cards grid

--- components/ui/card-hover-effect.tsx
+++ components/ui/card-hover-effect.tsx
@@ -52,7 +52,7 @@ export const HoverEffect = ({
   return (
     <div
       className={cn(
-        'grid place-items-center rounded-3xl border border-gray-200 sm:grid-cols-2 lg:grid-cols-3',
+        'grid place-items-center sm:grid-cols-2 lg:grid-cols-3',
         className
       )}
     >
