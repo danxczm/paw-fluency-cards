6bf42c2f4d64c572d5abcecf8d440d26e0dbe8d8
dan <81313913+danxczm@users.noreply.github.com>
2024-04-22 16:39:07 +0300

    feat: let next js to work with all web pictures resources

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['images.unsplash.com', 'i.ibb.co', 'static.vecteezy.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
   },
 };
 
