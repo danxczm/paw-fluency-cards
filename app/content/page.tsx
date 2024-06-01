621dd422e1e055762c404ea0ee9cf4cb9e8cce88
dan <81313913+danxczm@users.noreply.github.com>
2024-06-01 15:37:42 +0300

    feat: added delete button on content screen

--- app/content/page.tsx
+++ app/content/page.tsx
@@ -6,6 +6,7 @@ import { readFlashCard } from './actions';
 
 import ContentForm from '@/components/content/flash-card-form';
 import { HoverEffect } from '@/components/ui/card-hover-effect';
+import DeleteButton from '@/components/ui/delete-button';
 
 const Content = async () => {
   const { data, error } = await readUserSession();
@@ -16,8 +17,9 @@ const Content = async () => {
 
   return (
     <section className='mx-auto max-w-7xl p-8'>
-      <div className='flex'>
+      <div className='flex space-x-4'>
         <ContentForm />
+        <DeleteButton items={flashCards} />
       </div>
       <HoverEffect items={flashCards} className='mt-4' />
     </section>
