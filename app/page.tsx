4c994f7ab6204406f2aca9bd949d30e7690be74e
dan <81313913+danxczm@users.noreply.github.com>
2024-06-06 23:54:34 +0300

    feat: working on hero section main page

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import NavMobile from '@/components/home/nav-mobile';
 import readUserSession from '@/lib/supabase/actions';
 import { redirect } from 'next/navigation';
 import { Logout } from './auth/actions';
+import { MaxWidthWrapper } from '@/components/ui/max-with-wrapper';
 
 export default async function Home() {
   const { data: userSession } = await readUserSession();
@@ -20,7 +21,24 @@ export default async function Home() {
         logout={logOutUser}
       />
       <NavMobile userSession={userSession} />
-      <h1>This is where we start...</h1>
+      <MaxWidthWrapper>
+        <section className='h-screen'>
+          <div className='mt-24 flex flex-col items-center justify-center p-2'>
+            <p className='translate-y-2 bg-gradient-to-r from-sky-900 to-cyan-400 bg-clip-text text-5xl font-extrabold text-transparent'>
+              Paw
+            </p>
+            <p className='bg-gradient-to-r from-cyan-500 to-sky-700 bg-clip-text py-2 text-5xl font-extrabold text-transparent'>
+              Fluency
+            </p>
+            <p className='-translate-y-2 bg-gradient-to-r from-sky-600  to-sky-500 bg-clip-text text-3xl font-extrabold text-transparent'>
+              cards
+            </p>
+            <p className='-translate-y-4 bg-gradient-to-r from-sky-600  to-sky-500 bg-clip-text text-xs text-transparent'>
+              education
+            </p>
+          </div>
+        </section>
+      </MaxWidthWrapper>
     </main>
   );
 }
