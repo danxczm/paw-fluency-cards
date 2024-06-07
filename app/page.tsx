0ded80f9c5726922e4b3cebd83985e4198c8035f
dan <81313913+danxczm@users.noreply.github.com>
2024-06-07 10:03:28 +0300

    feat: changed hero title look

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,8 @@ import { redirect } from 'next/navigation';
 import { Logout } from './auth/actions';
 import { MaxWidthWrapper } from '@/components/ui/max-with-wrapper';
 
+import { PawPrint } from 'lucide-react';
+
 export default async function Home() {
   const { data: userSession } = await readUserSession();
 
@@ -23,17 +25,22 @@ export default async function Home() {
       <NavMobile userSession={userSession} />
       <MaxWidthWrapper>
         <section className='h-screen'>
-          <div className='mt-24 flex flex-col items-center justify-center p-2'>
-            <p className='translate-y-2 bg-gradient-to-r from-sky-900 to-cyan-400 bg-clip-text text-5xl font-extrabold text-transparent'>
+          <div className='mt-24 flex flex-col items-center justify-center p-2 text-[#184363] '>
+            <PawPrint
+              className='h-12 w-12 translate-y-6'
+              strokeWidth={1.5}
+              absoluteStrokeWidth
+            />
+            <p className='translate-y-2 text-6xl font-extrabold'>
               Paw
             </p>
-            <p className='bg-gradient-to-r from-cyan-500 to-sky-700 bg-clip-text py-2 text-5xl font-extrabold text-transparent'>
+            <p className=' py-2 text-6xl font-extrabold'>
               Fluency
             </p>
-            <p className='-translate-y-2 bg-gradient-to-r from-sky-600  to-sky-500 bg-clip-text text-3xl font-extrabold text-transparent'>
+            <p className='-translate-y-2 text-3xl font-extrabold'>
               cards
             </p>
-            <p className='-translate-y-4 bg-gradient-to-r from-sky-600  to-sky-500 bg-clip-text text-xs text-transparent'>
+            <p className='-translate-y-4  bg-clip-text text-xs'>
               education
             </p>
           </div>
